3a5ca8e2649f7a7aa8be89db1d67aa453532933d
BaekGeunYoung <dvmflstm@naver.com>
2020-05-30 19:03:17 +0900

    redis distributed lock 실습 코드 작성중

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+import org.redisson.Redisson
+import org.redisson.config.Config
+import java.util.concurrent.TimeUnit
+
+fun main() {
+    val config = Config()
+    config.useSingleServer().address = "redis://localhost:6379"
+    val redissonClient = Redisson.create(config)
+
+    val start = System.currentTimeMillis()
+    for (i in (0 until 10000)) {
+        val lock = redissonClient.getLock("myLock")
+        if (lock.tryLock(100, 1000, TimeUnit.MILLISECONDS)) {
+            try {
+                val value = redissonClient.getAtomicLong("key")
+                println(value.incrementAndGet())
+            } finally {
+                lock.unlock()
+            }
+        }
+    }
+
+    val end = System.currentTimeMillis()
+    println("result : ${redissonClient.getAtomicLong("key").get()}")
+    println("elapsed time : ${end - start}ms")
+}
